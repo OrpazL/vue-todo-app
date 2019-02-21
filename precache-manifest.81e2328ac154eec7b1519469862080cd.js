e3053972eeb9d3701c6045794e8b9760aa730502
OrpazL <orpaz.lazar@gmail.com>
2019-02-21 13:48:58 +0200

    Initial dist subtree commit

---
+++ precache-manifest.81e2328ac154eec7b1519469862080cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a92ab43634298330121",
+    "url": "/vue-todo-app/css/app.e32b6ccc.css"
+  },
+  {
+    "revision": "1a92ab43634298330121",
+    "url": "/vue-todo-app/js/app.22bbb0ba.js"
+  },
+  {
+    "revision": "fe7524f3ead8c4ca0f61",
+    "url": "/vue-todo-app/js/chunk-vendors.42d262b6.js"
+  },
+  {
+    "revision": "8bb3279a571d55e0ec61ba3ff7c8f066",
+    "url": "/vue-todo-app/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-todo-app/robots.txt"
+  }
+];
